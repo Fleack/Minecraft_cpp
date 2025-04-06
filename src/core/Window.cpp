131ff05c4d59d5025e183d01666c929d5322a0dc
Gleb Melnik <339891@niuitmo.ru>
2025-04-06 17:18:19 +0300

    Fix incorrect block rendering by enabling depth testing and setting depth bits to 24

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -54,6 +54,8 @@ bool Window::initGLFW()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_DEPTH_BITS, 24);
+    glEnable(GL_DEPTH_TEST);
     return true;
 }
 
