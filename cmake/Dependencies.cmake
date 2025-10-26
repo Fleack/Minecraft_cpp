614a38f516de21b03c13f4e15beca6dd2957ab22
Gleb Melnik <339891@niuitmo.ru>
2025-10-26 12:08:40 +0300

    Add ENet library

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,3 +1,9 @@
+# ENet configuration
+set(ENET_STATIC ON CACHE BOOL "" FORCE)
+set(ENET_SHARED OFF CACHE BOOL "" FORCE)
+set(ENET_TEST OFF CACHE BOOL "" FORCE)
+add_subdirectory(extern/enet)
+
 include_directories(extern/ska_sort EXCLUDE_FROM_ALL)
 
 find_package(SDL2 REQUIRED)
